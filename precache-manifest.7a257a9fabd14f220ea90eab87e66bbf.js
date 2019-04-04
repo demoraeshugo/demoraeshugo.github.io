1572e01ba9edc4ee6db76b3c1eb0a862cf4e5bf4
Hugo De Moraes <School@Hugos-MBP.fios-router.home>
2019-04-04 02:22:39 -0400

    Updates

---
+++ precache-manifest.7a257a9fabd14f220ea90eab87e66bbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "835dce195f4d4f587107",
+    "url": "/static/css/main.b8cdeae4.chunk.css"
+  },
+  {
+    "revision": "835dce195f4d4f587107",
+    "url": "/static/js/main.ef72d522.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e44ae84d3acd900872b7",
+    "url": "/static/css/2.0439a0c8.chunk.css"
+  },
+  {
+    "revision": "e44ae84d3acd900872b7",
+    "url": "/static/js/2.25abc716.chunk.js"
+  },
+  {
+    "revision": "e21f1cf0ab0a90f2b064ad58d564139b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.116c92323c34eeeffb4f376db5863952.js"
+  "/precache-manifest.7a257a9fabd14f220ea90eab87e66bbf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-app/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
